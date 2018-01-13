9012ab506962aba31181b09873a9c4f95e099ca1
Adam M Deeley <ad.deeley@gmail.com>
2018-01-13 22:06:40 +0000

    Initial Commit.

--- js/src/index.js
+++ js/src/index.js
@@ -1,8 +1,3 @@
 window.onload = function() {
-    let content = document.createElement("div"),
-        para = document.createElement("p"),
-        body = document.getElementsByTagName("body")[0];
-
-    para.innerHTML = "Hello, World!",
-    body.appendChild(content.appendChild(para));
+    
 }
